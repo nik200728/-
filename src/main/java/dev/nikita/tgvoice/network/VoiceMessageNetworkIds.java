69378ae28f7c727eb4180f029adeabb087cf040c
nik200728 <kovqwerty@gmail.com>
2026-08-28 23:31:59 +0600

    Add stable Voice Message network identifiers

---
+++ src/main/java/dev/nikita/tgvoice/network/VoiceMessageNetworkIds.java
@@ -0,0 +1,10 @@
+package dev.nikita.tgvoice.network;
+
+/** Stable identifiers for the Voice Message transport protocol. */
+public final class VoiceMessageNetworkIds {
+    public static final String SEND = "send_voice_message";
+    public static final String DELIVER = "deliver_voice_message";
+    public static final String REQUEST = "request_voice_message";
+
+    private VoiceMessageNetworkIds() {}
+}
