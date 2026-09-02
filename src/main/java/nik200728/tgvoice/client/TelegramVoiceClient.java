60b23672628e73fe7403ea3bbd83a4b1406cb5de
nik200728 <kovqwerty@gmail.com>
2026-09-02 22:43:17 +0600

    Fix client bootstrap imports and input controller

--- src/main/java/nik200728/tgvoice/client/TelegramVoiceClient.java
+++ src/main/java/nik200728/tgvoice/client/TelegramVoiceClient.java
@@ -1,7 +1,9 @@
 package nik200728.tgvoice.client;
 
 import dev.nikita.tgvoice.client.PlasmoVoiceClientAddon;
+import dev.nikita.tgvoice.client.VoiceMessageInput;
 import dev.nikita.tgvoice.client.VoiceMessagePlaybackManager;
+import dev.nikita.tgvoice.client.VoiceMessageUiController;
 import net.fabricmc.api.ClientModInitializer;
 import su.plo.voice.api.client.PlasmoVoiceClient;
 
@@ -14,7 +16,7 @@ public final class TelegramVoiceClient implements ClientModInitializer {
         // Plasmo Voice owns the microphone lifecycle. We only observe its processed
         // capture event while a Voice Message is being recorded.
         PlasmoVoiceClient.getAddonsLoader().load(new PlasmoVoiceClientAddon());
-        VoiceMessageInputController.register();
+        VoiceMessageInput.register();
         VoiceMessagePlaybackManager.register();
         VoiceMessageUiController.register();
     }
