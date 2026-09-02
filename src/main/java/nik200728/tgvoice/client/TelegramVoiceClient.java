7a62256a260e4b66a9bf1c796afb3a617541d88a
nik200728 <kovqwerty@gmail.com>
2026-09-03 03:00:35 +0600

    Fix duplicate client initializer import

--- src/main/java/nik200728/tgvoice/client/TelegramVoiceClient.java
+++ src/main/java/nik200728/tgvoice/client/TelegramVoiceClient.java
@@ -7,7 +7,6 @@ import dev.nikita.tgvoice.client.VoiceMessageInput;
 import dev.nikita.tgvoice.client.VoiceMessagePlaybackManager;
 import dev.nikita.tgvoice.network.VideoNotePayload;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import su.plo.voice.api.client.PlasmoVoiceClient;
